c3d22d905520de249327ee8bdf32abcec378b70c
Danilo Anselmo <anselmodanilo03@gmail.com>
2026-08-31 17:10:49 -0300

    aula finalizada

--- Aulas/aula16ex/ex018/script.js
+++ Aulas/aula16ex/ex018/script.js
@@ -1,12 +1,31 @@
-function adicionar() {
-    let num = Number(document.getElementById('txtnum').value)
-    let resp = document.getElementById('res')
-    let tab = document.getElementById('seltab')
-    let array = num.push(num)
+let num = document.getElementById('txtnum')
+let tab = document.getElementById('seltab')
+let resp = document.getElementById('res')
+let valores = []
 
-    if (num.length == 0  || num < 1 || num > 100 || num == array) {
-        window.alert('Valor inválido ou já encontrado na lista!')
+
+function isNumero(n) {
+    if(Number(n) >= 1 && Number(n) <= 100) {
+        return true
     } else {
+        return false
+    }
+}
+
+function inLista(n, l) {
+    if (l.indexOf(Number(n)) != -1) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function adicionar() {
+
+    if (isNumero(num.value) && !inLista(num.value)) {
+
+    } else {
+        window.alert('Valor inválido ou já encontrado na lista!')
         let item = document.createElement('option')
         item.text += `Valor ${num} adicionado.`
         tab.appendChild(item)
